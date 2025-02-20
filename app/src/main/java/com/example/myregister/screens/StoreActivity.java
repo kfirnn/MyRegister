49e59e5983c348ec79dd6a62836904c54095939c
kfirnn <kfirn5566@gmail.com>
2025-02-20 12:44:11 +0200

    init

--- app/src/main/java/com/example/myregister/screens/StoreActivity.java
+++ app/src/main/java/com/example/myregister/screens/StoreActivity.java
@@ -12,15 +12,5 @@ import com.example.myregister.R;
 
 public class StoreActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_store);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
 }
